4183142e89ef95edf26cd4807f9101be0e43835b
djuodis <deividas73245@gmail.com>
2023-11-19 05:58:16 +0200

    init

---
+++ script.js
@@ -0,0 +1,90 @@
+// 1. Užduotis
+
+const car = 'audi'   
+
+switch(car) {
+    case 'BMW':
+    case 'Mini':
+    case 'Rolls-Royce':
+   alert ('BMW Group')
+break
+    case 'VW':
+    case 'Audi':
+    case 'Bentley':
+    case 'Bugatti':
+    case 'Lamborghini':
+    case 'Porsche':
+    alert ('VW Group')
+break
+    default:alert('Kita automobilių grupė')
+}
+
+
+// 2. Užduotis
+
+const userInput  = "Obuolys"
+
+switch(userInput) {
+    case 'Obuolys':
+    case 'Braškė':
+    case 'agrastas':
+    case 'avietė':
+    case 'bruknė':
+   alert ('Vaisius')
+break
+    case 'Bulvė':
+    case 'Topinambas':
+    case 'Rabarbaras':
+    case 'Briuselio kopūstas':
+    case 'Brokolis':
+    case 'kopūstas':
+    alert ('Daržovė')
+}
+
+
+
+// 3. Užduotis
+
+const weekDay = '6'
+switch(weekDay) {
+        case '0':
+        alert ('Pirmadienis')
+        console.log('Pirmadienis')
+break
+}
+switch(weekDay) {
+    case '1':
+        alert ('Antradienis')
+        console.log('Antradienis')
+break
+   }
+   switch(weekDay) {
+    case '2':
+       alert ('Trečiadienis')
+       console.log('Trečiadienis')
+break
+}
+switch(weekDay) {
+    case '3':
+       alert ('Ketvirtadienis')
+       console.log('Ketvirtadienis')
+break
+}
+switch(weekDay) {
+    case '4':
+       alert ('Penktadienis')
+       console.log('Penktadienis')
+break
+}
+switch(weekDay) {
+    case '5':
+       alert ('Šeštadienis')
+       console.log('Šeštadienis')
+break
+}
+switch(weekDay) {
+    case '6':
+       alert ('Sekamadienis')
+       console.log('Sekamadienis')
+break
+}
